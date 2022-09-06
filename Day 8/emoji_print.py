762c6fe83dfd1514ada35931d1c0185807ac949c
Marvellordz Mega <107189201+Marvellordz@users.noreply.github.com>
2022-09-06 14:05:59 +0100

    Created emoji_print

---
+++ Day 8/emoji_print.py
@@ -0,0 +1,8 @@
+import emoji
+
+print(emoji.emojize("I was born a champion :1st_place_medal:"))
+print(emoji.emojize("Where do broken hearts :broken_heart: go?"))
+
+# You can get the full list of emojis and their names here: https://carpedm20.github.io/emoji/
+# Just insert the emoji name in-between two colons (::) E.g :red_heart: will produce ❤️
+emoji.emojize(":love:")
